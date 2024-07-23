289ab733aa22683092f5ec02a58f2c80de30ea91
SergioVelasquez-D <109398726+SergioVelasquez-D@users.noreply.github.com>
2024-07-23 12:37:23 -0500

    2commit
    Categorias View

--- app/src/main/java/com/sergio/ecoreciclaapp/CategoriasActivity.java
+++ app/src/main/java/com/sergio/ecoreciclaapp/CategoriasActivity.java
@@ -8,6 +8,6 @@ public class CategoriasActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_configuration);
+        setContentView(R.layout.activity_categories);
     }
 }
